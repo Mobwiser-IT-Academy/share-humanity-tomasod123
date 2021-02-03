99f9ca6c6e4a29f4732a913a8c1d6a81b74631e1
tomasod123 <tmsdwyer50@gmail.com>
2021-02-03 15:23:24 +0000

    seperated crises into different sections

--- app.js
+++ app.js
@@ -6,24 +6,30 @@ app.set("view engine", "ejs");
 
 const issuesList = [
   {
+    category: "health",
     title: "Violence, Cholera, Hunger, Coronavirus",
     location: "Yemen",
     peopleAffected: "24,000,000",
     rank: "1",
   },
   {
+    category: "social",
     title: "Syrian Civil War",
     location: "Syria",
     peopleAffected: "12,000,000",
     rank: "2",
   },
   {
+    category: "social",
     title: "Armed Conflicts, Food Insecurity, Epidemics",
     location: "Democratic Republic of Congo",
     peopleAffected: "15,740,000",
     rank: "3",
   },
 ];
+
+//THIS IS WHERE WE'RE TELLING OUR SERVER (APP.JS WHERE TO LOOK FOR RESOURCES)
+app.use(express.static(__dirname + "/public"));
 
 //This is the section that is connecting us to "home.ejs"
 app.get("/", (req, res) => {
--- script.js
+++ script.js
@@ -1,7 +1,20 @@
-
-
-
- "crises":[
-     {"title":"Violence, Cholera, Hunger, Coronavirus", "Location": "Yemen", "People Affected": "24,000,000", "Rank":"1", },
-     {"title":"Syrian Civil War", "Location": "Syria", "People Affected": "12,000,000", "Rank":"2", },
-   {"title":"Armed Conflicts, Food Insecurity, Epidemics", "Location": "Democratic Republic of Congo", "People Affected": "15,740,000", "Rank":"3", },]
+let issues = [
+  {
+    title: "Violence, Cholera, Hunger, Coronavirus",
+    Location: "Yemen",
+    "People Affected": "24,000,000",
+    Rank: "1",
+  },
+  {
+    title: "Syrian Civil War",
+    Location: "Syria",
+    "People Affected": "12,000,000",
+    Rank: "2",
+  },
+  {
+    title: "Armed Conflicts, Food Insecurity, Epidemics",
+    Location: "Democratic Republic of Congo",
+    "People Affected": "15,740,000",
+    Rank: "3",
+  },
+];
